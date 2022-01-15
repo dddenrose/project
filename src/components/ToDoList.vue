6762cf21088fef6987e79255a5dbd781878b98a2
Chi Yang <dddenrose@gmail.com>
2022-01-15 16:53:26 +0800

    first commit

---
+++ src/components/ToDoList.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="ToDoList">
+    <div id="todo">
+      <div class="container">
+        <h1>todo</h1>
+        <input type="text" v-model="inputData">
+        <button class="add" @click="addItemToList">Add</button>
+        <ul>
+          <li v-for="(item, index) in list" :key="index">{{item}}
+            <button class="delet" @click="deletItem(index)">delet</button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ToDoList',
+  data () {
+    return {
+      list: [],
+      inputData: ''
+    } 
+  },
+  methods:{
+    addItemToList() {
+      this.list.push(this.inputData)
+      console.log(this.list)
+    },
+    deletItem(key) {
+      this.list.splice(key,1)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  li {
+    list-style-type: none;
+  }
+
+</style>
